ae643e4685e89d45c4516fd367bd2633516154eb
katguz3485 <katarzyna.guzik01@gmail.com>
2018-07-25 16:38:57 +0200

    spec students fixed

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :student do
     first_name { Faker::Name.first_name}
     last_name {Faker::Name.last_name}
-    birthdate {Faker::Date.birthdate}
+    birthdate {Faker::Date.birthday}
 
   end
 
