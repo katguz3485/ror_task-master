b6d10a43c09e51c04b711c4450f973d913f60dbf
katguz3485 <katarzyna.guzik01@gmail.com>
2018-08-16 13:08:17 +0200

    production small changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+    # Code is not reloaded between requests.
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.assets.digest = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
