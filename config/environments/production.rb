8b302284a275d1e58fa91daae2ce7c18890f4b68
katguz3485 <katarzyna.guzik01@gmail.com>
2019-01-05 23:40:50 +0100

    [Fix] db confifg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,6 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
   #
   #
-
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
